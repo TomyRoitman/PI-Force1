ffa426c5547ef256a54aef9642d47d895f461505
XxBoomerxX <tomyrm30@gmail.com>
2021-05-26 03:05:38 +0300

    Added image matching and started printing distance to screen:

--- main_client.py
+++ main_client.py
@@ -5,17 +5,20 @@ import threading
 import time
 
 import cv2
+import imagehash
 import numpy as np
 import pygame
+from PIL import Image
 
 from gui import Gui, Commands
 from image_processing.distance import DistanceCalculator2
-from image_processing.object_detection import ObjectDetector
+from image_processing.object_detection import ObjectDetector, DetectionResult
 from image_processing.stereo import StereoDepthMap
 from network.communication import TCPStream
 from network.protocol import PICommunication
 from network.stream_receiver import StreamReceiver
 
+CUTOFF = 50
 CAMERA_MENU_TEXT = """
 [1] Left Camera\n
 [2] Right Camera\n
@@ -85,17 +88,69 @@ def create_depth_map():
             # cv2.imshow("Depth Map", depth_map)
             DEPTH_MAP_QUEUE.append(depth_map)
         cv2.waitKey(1)
-        time.sleep(1.0 / 40)
+        time.sleep(1.0 / 60)
     sys.exit()
 
 
 def blit_frame(screen, frame, size, position):
     frame_blit = cv2.resize(frame, size)
-    # frame_blit = cv2.flip(frame_blit, 1)  # flip horizontal
+    frame_blit = cv2.flip(frame_blit, 1)  # flip horizontal
     frame_blit = cv2.cvtColor(frame_blit, cv2.COLOR_BGR2RGB)
     frame_blit = np.rot90(frame_blit)
     surface = pygame.surfarray.make_surface(frame_blit)
-    screen.blit(surface, position)
+    if RUNNING:
+        screen.blit(surface, position)
+
+
+def compare_images(frame0, frame1):
+
+    try:
+        im0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2RGB)
+        im0 = Image.fromarray(im0)
+        hash0 = imagehash.average_hash(im0)
+        im1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+        im1 = Image.fromarray(im1)
+        hash1 = imagehash.average_hash(im1)
+        to_return = (hash0 - hash1) < CUTOFF
+        return to_return
+    except:
+        return False
+
+
+def get_object_from_detection_result(frame: np.ndarray, detection_result: DetectionResult):
+    return frame[detection_result.location[1]:detection_result.location[3],
+           detection_result.location[0]:detection_result.location[2]]
+
+
+def find_object_pairs(left_frame, left_results, right_frame, right_results, distance_calculator: DistanceCalculator2):
+    right_obj_types = list(map(lambda result: result.obj_type, right_results))
+    for left_obj in left_results:
+        if not left_obj.obj_type in right_obj_types:
+            continue
+        for right_obj in right_results:
+            if left_obj.obj_type == right_obj.obj_type:
+                left_cropped_obj = get_object_from_detection_result(left_frame, left_obj)
+                right_cropped_obj = get_object_from_detection_result(right_frame, right_obj)
+                if left_cropped_obj is None or right_cropped_obj is None:
+                    continue
+                if compare_images(left_cropped_obj, right_cropped_obj):
+                    l_x = (left_obj.location[0] + left_obj.location[2]) / 2.0
+                    r_x = (right_obj.location[0] + right_obj.location[2]) / 2.0
+                    print(l_x, r_x)
+                    distance = distance_calculator.calculate_distance(l_x, r_x)
+                    print("Distance from person: ", distance)
+                    distance_string = f" distance: {distance}"
+                    left_obj.label += distance_string
+                    right_obj.label += distance_string
+
+
+def put_results_on_frame(frame, results):
+    for result in results:
+        (startX, startY, endX, endY) = result.location
+        cv2.rectangle(frame, (startX, startY), (endX, endY), result.color, 2)
+        y = startY - 15 if startY - 15 > 15 else startY + 15
+        cv2.putText(frame, result.label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, result.color, 2)
+
 
 
 def handle_stream(constants, screen):
@@ -112,70 +167,90 @@ def handle_stream(constants, screen):
     left_results = []
     right_results = []
     # screen.fill(BACKGROUND_COLOR)
-    while RUNNING:
+    left_frame = None
+    right_frame = None
+    running = RUNNING
+    while running:
 
-        current_frames = []
+        # Get left frame
         if receiver1.frame_queue:
             left_ret = True
             LOCK.acquire()
             left_frame = receiver1.frame_queue.pop(0)
             LOCK.release()
-            current_frames.append(left_frame)
             frame, left_results = detector.detect(left_frame)
-
-            # h, w = frame.shape[:2]
-            # cv2.line(frame, (0, int(h / 2) - 2), (w - 1, int(h / 2) - 2), (0, 0, 0), 2)
-            # cv2.line(frame, (int(w / 2) - 2, 0), (int(w / 2) - 2, h - 1), (0, 0, 0), 2)
-
-            blit_frame(screen, left_frame, (640, 480), (SCREEN_DIMENSIONS[0] / 2, 50))
 
         else:
             left_ret = False
 
+        # Get right frame
         if receiver2.frame_queue:
             right_ret = True
+            LOCK.acquire()
             right_frame = receiver2.frame_queue.pop(0)
-            current_frames.append(right_frame)
+            LOCK.release()
             frame, right_results = detector.detect(right_frame)
-
-            # h, w = frame.shape[:2]
-            # cv2.line(frame, (0, int(h / 2) - 2), (w - 1, int(h / 2) - 2), (0, 0, 0), 2)
-            # cv2.line(frame, (int(w / 2) - 2, 0), (int(w / 2) - 2, h - 1), (0, 0, 0), 2)
-            size = (640, 480)
-            blit_frame(screen, frame, size, (SCREEN_DIMENSIONS[0] / 2 - size[0], 50))
 
         else:
             right_ret = False
 
+        # Handle stereo cases
         if left_ret and right_ret:
+
+            # Create depth map
             LOCK.acquire()
-            FRAME_QUEUE.append(current_frames)
+            FRAME_QUEUE.append((left_frame, right_frame))
             if DEPTH_MAP_QUEUE:
                 depth_map = DEPTH_MAP_QUEUE.pop(0)
-                size = (300, 225)
-                blit_frame(screen, depth_map, size, ((SCREEN_DIMENSIONS[0] - size[0]) / 2, 550))
+                if RUNNING:
+                    depth_map_size = (300, 225)
+                    blit_frame(screen, depth_map, depth_map_size, ((SCREEN_DIMENSIONS[0] - depth_map_size[0]) / 2, 550))
+                else:
+                    break
             LOCK.release()
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # # left_results = list(filter(lambda a: "person" in a.label, left_results))
+            # # right_results = list(filter(lambda a: "person" in a.label, right_results))
+            # pairs, left_results, right_results = find_object_pairs(left_results, right_results)
+            # if left_results and right_results:
+            #     l_result = left_results[0]
+            #     r_result = right_results[0]
 
-        left_results = list(filter(lambda a: "person" in a.label, left_results))
-        right_results = list(filter(lambda a: "person" in a.label, right_results))
-        if left_results and right_results:
-            l_result = left_results[0]
-            r_result = right_results[0]
-            l_x = (l_result.location[0] + l_result.location[2]) / 2.0
-            r_x = (r_result.location[0] + r_result.location[2]) / 2.0
-            print("Distance from person: ", distance_calculator.calculate_distance(l_x, r_x))
+            # Handle object detection and distance measurement
+            find_object_pairs(left_frame, left_results, right_frame, right_results,
+                              distance_calculator)  # Calculate distance from objects
 
-        # screen.blit(left_surface, (500, 0))
-        # screen.blit(right_surface, (0, 0))
+        if left_frame is not None:
+            if left_results:
+                put_results_on_frame(left_frame, left_results)
+            left_frame_size = (640, 480)
+            blit_frame(screen, left_frame, left_frame_size, (SCREEN_DIMENSIONS[0] / 2, 50))
+        if right_frame is not None:
+            if right_results:
+                put_results_on_frame(right_frame, right_results)
+            right_frame_size = (640, 480)
+            blit_frame(screen, right_frame, right_frame_size, (SCREEN_DIMENSIONS[0] / 2 - right_frame_size[0], 50))
+
+        if not RUNNING:
+            pygame.quit()
+            sys.exit()
+
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.JOYBUTTONDOWN and event.button == 7):
                 pygame.quit()
                 sys.exit()
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+        LOCK.acquire()
+        running = RUNNING
+        LOCK.release()
+
+
         # time.sleep(1.0 / 24)
+
+    pygame.quit()
+    sys.exit()
 
 
 def main():
