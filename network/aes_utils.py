ca0e3600d0007c2c1df8b090311eeae4affaa1bf
XxBoomerxX <tomyrm30@gmail.com>
2021-05-29 17:48:26 +0300

    Added debug prints

--- network/aes_utils.py
+++ network/aes_utils.py
@@ -13,6 +13,7 @@ class AESCipher(object):
     def encrypt(self, raw):
         print(raw)
         raw = self._pad(raw)
+        print("Padded: ", raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return base64.b64encode(iv + cipher.encrypt(raw))
@@ -20,12 +21,14 @@ class AESCipher(object):
     def decrypt(self, enc):
         print(enc)
         enc = base64.b64decode(enc)
+        print("b64 Decoded: ", enc)
         iv = enc[:AES.block_size]
         print(len(enc), len(iv))
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return self._unpad(cipher.decrypt(enc[AES.block_size:]))
 
     def _pad(self, s):
+        print(chr(self.bs - len(s) % self.bs).encode())
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs).encode()
 
     @staticmethod
